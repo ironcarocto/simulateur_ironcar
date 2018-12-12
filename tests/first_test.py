4287609fde5b9434903a81a5e33ec3ca04ff3a08
Constant Bridon <constant.bridon@Constants-MacBook-Pro.local>
2018-12-12 16:54:33 +0100

    journée R&D

--- tests/first_test.py
+++ tests/first_test.py
@@ -1,5 +1,5 @@
 from simulateur.picture_generation import Point
-from simulateur.picture_generation import compute_command_arc
+from simulateur.picture_generation import compute_command_line
 
 
 def test_first_to_fail():
@@ -14,3 +14,10 @@ def test_compute_angle():
     end = Point(800, 0)
     radius = 2000
     compute_command_arc(origin, end, radius)
+
+
+
+def test_compute_line():
+    origin = Point(500, 1000)
+    end = Point(600, 0)
+    compute_command_line(origin, end)
