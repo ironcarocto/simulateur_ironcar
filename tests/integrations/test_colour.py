10726840f210c28dc3d52c43e4909afe5810464b
Fabien Arcellier <farcellier@octo.com>
2019-04-24 00:14:42 +0200

    suppression des tests d'intégration hors scope

--- tests/integrations/test_colour.py
+++ tests/integrations/test_colour.py
@@ -1,16 +1,6 @@
 import pytest
 from colour import Color, hex2rgb
 
-def test_Color_constructor_parse_white_color_from_hexadecimal_value():
-    c = Color('#ffffff')
-    assert c.rgb == (1.0, 1.0, 1.0)
-
-
-def test_Color_constructor_raise_an_error_when_color_value_is_invalid():
-    with pytest.raises(AttributeError, match="'#ff' is not in web format. Need 3 or 6 hex digit."):
-        Color('#ff')
-
-
 def test_hex2rgb_convert_white_color_into_rgb_tuple():
     rgb = hex2rgb('#ffffff')
     assert rgb == (1.0, 1.0, 1.0)
@@ -26,4 +16,5 @@ def test_pattern_to_check_color_value_is_hexadecimal():
     try:
         hex2rgb(value)
     except ValueError:
+        # show error message here
         pytest.fail('color is not valid %s'.format(value))
